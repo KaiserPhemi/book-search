f8c777979a1da50b1aebe2966d31f761a4af855f
KaiserPhemi <oluwafemi.akinwa@andela.com>
2019-12-26 15:07:00 +0100

    chore(init): initialize project
    - add react libraries

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// react libraries
+import React from "react";
+import { render } from "react-dom";
+
+const Index = () => {
+  return <div>Hello there</div>;
+};
+
+render(<Index />, document.getElementById("main-app"));
